ebc37804eb12a4c22bad464536043fa7ee7ec973
hamiltongardeninnsuites-prog <hamiltongardeninnsuites@gmail.com>
2026-09-15 10:57:39 +0530

    databse changes

--- HamiltonServer/config/database.js
+++ HamiltonServer/config/database.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
     mongoose.set("strictQuery", false);
-    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`).then((data) => {
-        console.log(`MongoDb connected with server: ${data.connection.host} `);
-    }).catch((err) => {
-        console.log(err);
-    });
-}
 
-module.exports = connectDatabase
+    mongoose
+        .connect(process.env.MONGO_URI)
+        .then((data) => {
+            console.log(`MongoDb connected with server: ${data.connection.host}`);
+        })
+        .catch((err) => {
+            console.log("MongoDB connection error:", err);
+        });
+};
+
+module.exports = connectDatabase;
